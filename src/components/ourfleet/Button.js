769d7e173791fbf2a14591af026ac4bc4b3e7213
osman <osmanaziz012@gmail.com>
2023-11-13 08:30:01 -0800

    ...

--- src/components/ourfleet/Button.js
+++ src/components/ourfleet/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = () => {
   return (
-    <button className="font-Montserrat font-600 text-shadow m-32 mt-48 h-[74px] w-[280px] rounded-sm bg-[#A72211] text-[19px] uppercase text-[#FFF] hover:bg-red-800 focus:outline-none focus:ring focus:ring-red-500">
+    <button className="font-Montserrat text-shadow my-10 h-[58px] w-[170px] rounded-sm bg-[#A72211] text-[13px] font-[600] uppercase text-[#FFF] hover:bg-red-800 focus:outline-none focus:ring focus:ring-red-500 md:my-16 md:w-[170px] 1024:my-10 lg:h-[58px] lg:w-[160px] lg:text-[14px] 2xl:my-24 2xl:h-[100px] 2xl:w-[350px] 2xl:text-[25px]">
       See options
     </button>
   );
--- src/components/ourfleet/Section.js
+++ src/components/ourfleet/Section.js
@@ -4,35 +4,33 @@ import Button from "./Button";
 const Section = ({ order, textContent, title, imgURL }) => {
   return (
     <div
-      className={`flex flex-wrap ${
-        order === "right" ? "flex-row" : "flex-row-reverse"
+      className={`flex flex-col sm:flex-col md:flex-col lg:flex-row ${
+        order === "left" ? "flex-row-reverse" : "flex-row"
       }`}
     >
       <div
-        className={`w-full sm:w-full md:w-full lg:w-1/2 ${
-          order === "left" ? "lg:order-1" : "lg:order-2"
+        className={`flex w-full items-stretch sm:w-full md:w-full lg:w-[50%] ${
+          order === "right" ? "lg:order-1" : "lg:order-2"
         }`}
       >
-        <img src={imgURL} alt="Image" className="h-full w-full" />
+        <img src={imgURL} alt="Image" className="h-auto w-full" />
       </div>
       <div
-        className={`flex w-full flex-col items-center justify-center border-gray-100 bg-black sm:w-full sm:p-24 md:w-full md:p-32 lg:w-1/2 ${
+        className={`flex w-full flex-col items-center bg-black sm:w-full md:w-full lg:w-[50%] lg:justify-center ${
           order === "right" ? "lg:order-2" : "lg:order-1"
-        } lg:pt-20`}
+        } flex-1 overflow-hidden`}
       >
-        <h1 className="font-Montserrat mb-24 pt-32 text-center text-[33px] font-medium uppercase tracking-[14px] text-white">
+        <h1 className="font-Montserrat lg: my-16 text-center text-[24px] font-medium uppercase tracking-[7px] text-white md:my-14 1024:mb-10 2xl:my-24 2xl:text-[40px]">
           {title}
         </h1>
-
         {textContent.map((text, index) => (
           <React.Fragment key={index}>
-            <p className="font-Archivo px-5 text-center text-[24px] text-white">
+            <p className="font-Archivo mx-14 text-center text-[12px] text-white md:mx-20 md:text-[14px] 1024:mx-14 1024:text-[15px] 2xl:text-[25px]">
               {text}
             </p>
             {index < textContent.length - 1 && <br />}
           </React.Fragment>
         ))}
-
         <Button />
       </div>
     </div>
