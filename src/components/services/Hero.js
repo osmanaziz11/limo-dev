9bfe4486dc36e9d47f05b4bab466d202c4a4b48c
hadees <hadees621@gmail.com>
2023-11-25 16:37:06 +0500

    services page updated

--- src/components/services/Hero.js
+++ src/components/services/Hero.js
@@ -1,75 +1,55 @@
 import React from "react";
+import Image from "next/legacy/image";
+
+const servicesData = [
+  "Gold Key Concierge",
+  "Airport Transfers",
+  "Executive Ground Transportation",
+  "Tijuana/CBX Airport Services",
+  "Temecula Wine Tours",
+  "Wedding Limousine Transportation",
+  "San Diego Brewery Tours",
+  "Concert Limo Services",
+  "Mexico Limousine Tours",
+  "Bachelor & Bachelorette Parties",
+  "SoCal Casino Limo Services",
+  "Del Mar Races Limousine Service",
+  "Graduation Limo Services",
+  "Sporting Event Transportation",
+  "Prom Night Limousine Services",
+  "Quinceañera Limousine Service",
+  "Nightclub Limo Service",
+  "School Event Limo Services",
+];
 
 const Hero = () => {
   return (
     <>
-      <div class="relative">
-        <img
-          src="https://s3-alpha-sig.figma.com/img/2753/fb0f/48dd7a4e69830e52bf9c440c03b9a7dc?Expires=1701043200&Signature=NtB7uvLto28k9tLHIbupALSrRT0yfLD8mFj4u8ft~vutOzaTZh-ZrePS84fn8bWjrjPXO2scoFGneTzGpmEKsqzDfNf0U2GXq~KNg~XEgR7hIXeAI7gUeNTNT~laNaL7sKfwI2~f9IJiugKpFkNM2r-0jyaDP8T1T3vPhKlZpCraEc34PiiAGQuQcrUKYAzPW1uMmAyln3fJzcGm62TlGxqzICQYQagVNlYykepzE~J9g206ZgnK54e0y6TGkqrreoboVjEF9NQPgTI6fK1SP1YimUXDfi3Vb57GZoIexNqsX7gL1f0oRPkpUb4uwatGt6lPtngFon3r9uJtS3pzNQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-          alt=""
-          class="h-auto w-full max-w-full"
-        />
-        <div class="absolute left-[3%] top-[23%] sm:text-left">
-          <h1 class="font-Leaguespartan text-5xl font-extrabold -tracking-tighter text-white sm:text-2xl md:text-7xl">
+      <div className="relative 320:mb-2 768:mb-12 1024:mb-32">
+
+        <div className="relative w-full 320:h-[220px] 375:h-[290px] 425:h-[280px] 768:h-[550px] 1024:h-[830px] 1440:h-[1000px] 2xl:h-[1870px]">
+          <Image
+            src="https://res.cloudinary.com/dyjirwjvp/image/upload/q_60/v1700848233/image_6_xnlnzr.png"
+            blurDataURL="https://res.cloudinary.com/dyjirwjvp/image/upload/e_blur:100,q_10/v1700848233/image_6_xnlnzr.png"
+            layout="fill"
+          />
+        </div>
+        <div className="absolute left-[3%] top-[21%] 320:left-[5%] 425:top-[25%] 768:top-[23%] 1024:top-[23%] 1440:left-[5%] 1440:top-[23%] 2xl:top-[24%] 2xl:left-[6%]">
+
+          <h1 className="font-Leaguespartan font-[700] -tracking-tighter text-white 320:text-[12px] 320:leading-[11px] 768:text-[55px] 768:leading-[44px] 1024:text-[66px] 1024:leading-[52px] 1440:text-[90px] 1440:leading-[73px] 2xl:text-[142px] 2xl:leading-[115px]">
             EXCLUSIVE
             <br />
             SERVICES
           </h1>
-          <ul className="mt-6 space-y-2.5">
-            <li className="text-shadow-md font-Montserrat text-[14px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Gold Key Concierge
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[14px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Airport Transfers
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Executive Ground Transportation
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Tijuana/CBX Airport Services
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Temecula Wine Tours
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Wedding Limousine Transportation
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + San Diego Brewery Tours
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Concert Limo Services
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Mexico Limousine Tours
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Bachelor & Bachelorette Parties
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + SoCal Casino Limo Services
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Del Mar Races Limousine Service
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Graduation Limo Services
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Sporting Event Transportation
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Prom Night Limousine Services
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Quinceañera Limousine Service
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + Nightclub Limo Service
-            </li>
-            <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
-              + School Event Limo Services
-            </li>
+          <ul className="320:mt-2 320:space-y-1.5 768:space-y-4 768:mt-5 1024:space-y-[20px] 1024:mt-8 1440:space-y-6 1440:mt-8 2xl:space-y-12 2xl:mt-16">
+            {servicesData.map((service, index) => (
+              <li
+                key={index}
+                className=" text-white text-shadow-md font-Montserrat font-bold uppercase italic leading-normal 320:text-[4.5px] 320:leading-[1.2px] 320:tracking-[1.6px] 768:text-[12px] 768:leading-[0.8px] 768:tracking-[3.6px] 1024:text-[15px] 1024:leading-[3.5px] 1024:tracking-[5px] 1440:text-[21px] 1440:leading-[6px] 1440:tracking-[6px] 2xl:text-[35px] 2xl:leading-[4.6px] 2xl:tracking-[7px]"
+              >
+                + {service}
+              </li>
+            ))}
           </ul>
         </div>
       </div>
@@ -78,3 +58,86 @@ const Hero = () => {
 };
 
 export default Hero;
+
+
+
+// import React from "react";
+
+// const Hero = () => {
+//   return (
+//     <>
+//       <div class="relative">
+//         <img
+//           src="https://s3-alpha-sig.figma.com/img/2753/fb0f/48dd7a4e69830e52bf9c440c03b9a7dc?Expires=1701043200&Signature=NtB7uvLto28k9tLHIbupALSrRT0yfLD8mFj4u8ft~vutOzaTZh-ZrePS84fn8bWjrjPXO2scoFGneTzGpmEKsqzDfNf0U2GXq~KNg~XEgR7hIXeAI7gUeNTNT~laNaL7sKfwI2~f9IJiugKpFkNM2r-0jyaDP8T1T3vPhKlZpCraEc34PiiAGQuQcrUKYAzPW1uMmAyln3fJzcGm62TlGxqzICQYQagVNlYykepzE~J9g206ZgnK54e0y6TGkqrreoboVjEF9NQPgTI6fK1SP1YimUXDfi3Vb57GZoIexNqsX7gL1f0oRPkpUb4uwatGt6lPtngFon3r9uJtS3pzNQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+//           alt=""
+//           class="h-auto w-full max-w-full"
+//         />
+//         <div class="absolute left-[3%] top-[23%] sm:text-left">
+//           <h1 class="font-Leaguespartan text-5xl font-extrabold -tracking-tighter text-white sm:text-2xl md:text-7xl">
+//             EXCLUSIVE
+//             <br />
+//             SERVICES
+//           </h1>
+//           <ul className="mt-6 space-y-2.5">
+//             <li className="text-shadow-md font-Montserrat text-[14px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Gold Key Concierge
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[14px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Airport Transfers
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Executive Ground Transportation
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Tijuana/CBX Airport Services
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Temecula Wine Tours
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Wedding Limousine Transportation
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + San Diego Brewery Tours
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Concert Limo Services
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Mexico Limousine Tours
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Bachelor & Bachelorette Parties
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + SoCal Casino Limo Services
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Del Mar Races Limousine Service
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Graduation Limo Services
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Sporting Event Transportation
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Prom Night Limousine Services
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Quinceañera Limousine Service
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + Nightclub Limo Service
+//             </li>
+//             <li className="text-shadow-md font-Montserrat text-[16px] font-bold uppercase italic leading-normal tracking-[3.6px] text-white">
+//               + School Event Limo Services
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Hero;
--- src/components/services/Section.js
+++ src/components/services/Section.js
@@ -1,6 +1,17 @@
 import React from "react";
+import Image from "next/legacy/image";
 
-const Section = ({ order, textContent, title, imgURL, color, number, img }) => {
+const Section = ({
+  order,
+  textContent,
+  title,
+  imgURL,
+  blurImg,
+  SImg,
+  SImgBlur,
+  color,
+  number,
+}) => {
   const totalIcons = 17;
 
   const filledIcons = Array.from(
@@ -12,91 +23,205 @@ const Section = ({ order, textContent, title, imgURL, color, number, img }) => {
     { length: totalIcons - filledIcons.length },
     (_, i) => i,
   );
-
   return (
     <>
       <div
-        className={`flex h-[828px] w-full items-center justify-center bg-black ${
-          order === "left" ? "flex-row-reverse" : "flex-row"
-        }`}
+        className={`flex w-full justify-center bg-black 320:mt-10 768:mt-0 1024:h-[720px] 1440:h-[780px] 2xl:h-[980px] ${order === 'right' ? 'lg:flex-row' : 'lg:flex-row-reverse'}`}
       >
-        <div className={`bg-${color} flex h-[600px] w-full`}>
-          <div
-            className={`flex w-full items-center justify-center text-white  ${
-              order === "left" ? "flex-row-reverse pr-12" : "flex-row pl-12"
-            }`}
-          >
-            <div>
-              <div className="grid grid-cols-2">
-                <div className="flex justify-start">
-                  <h1
-                    className={`font-Leaguespartan mb-1 flex text-[70px] font-bold leading-[76.6%] ${
-                      order === "left" ? "pl-12" : ""
-                    }`}
-                  >{`${number}`}</h1>
-                </div>
-                <div className="mr-8 flex justify-end">
-                  <img src={`${img}`} alt="" class="h-[60px] w-[60px]" />
-                </div>
-              </div>
-              <h1
-                className={`font-Archivo mb-1 text-[35px] font-[400] italic ${
-                  order === "left" ? "pl-12" : ""
-                }`}
-              >{`${title}`}</h1>
-
-              <div className={`${order === "left" ? "pl-12" : "pr-10"}`}>
-                {textContent.map((text, index) => (
-                  <React.Fragment key={index}>
-                    <p className="font-Archivo text-[14px] font-[300] leading-[19.8px]">
-                      {text}
-                    </p>
-                    {index < textContent.length - 1 &&
-                      (number !== "17" ? <br /> : null)}
-                  </React.Fragment>
-                ))}
-              </div>
-            </div>
-            <img src={`${imgURL}`} alt="" className="h-[660px] w-[420px]" />
-          </div>
-        </div>
         <div
-          className={`bg-${color} flex h-[600px] w-[5%] flex-col items-center justify-center`}
+          className={`flex w-full flex-col items-center 320:h-[1250px] 425:h-[1250px] sm:flex-row 768:h-[1480px] md:w-full md:flex-col 1024:h-[600px] ${order === 'right' ? 'lg:flex-row' : 'lg:flex-row-reverse'}`}
         >
-          {filledIcons.map((index) => (
-            <svg
-              key={index}
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="28"
-              viewBox="0 0 22 34"
-              fill="white"
-              stroke="white"
-              strokeWidth="2"
-              style={{ margin: index % 2 === 0 ? "0px" : "-10px" }}
+          {/* 1st */}
+          <div
+            className={`bg-${color} flex w-full flex-col text-white 320:gap-4 320:py-5 1024:h-[600px] 1440:h-[680px] 2xl:h-[700px] 320:items-center ${order === 'right' ? 'lg:pl-14 lg:pr-12' : 'lg:pr-14 lg:pl-12'}`}
+          >
+            {/* number and logo */}
+            <div className="grid 320:grid-cols-1 768:grid-cols-1 1024:w-full 1024:grid-cols-2 items-center">
+              <div className="flex lg:justify-start">
+                <h1
+                  className={`font-Leaguespartan mb-1 flex text-[70px] font-bold leading-[76.6%] 320:flex-row`}
+                >
+                  {`${number}`}
+                </h1>
+              </div>
+
+            </div>
+
+            {/* title */}
+            <h1
+              className={`font-Archivo mb-1 font-[400] italic 320:text-[20px] 1024:w-full 1024:text-[35px]`}
             >
-              <path d="M1.99822 32.1138L2.22499 15.1476L20.0017 2.06227L19.775 19.0284L1.99822 32.1138Z" />
-            </svg>
-          ))}
-          {unfilledIcons.map((index) => (
-            <svg
-              key={index}
-              xmlns="http://w3.org/2000/svg"
-              width="16"
-              height="28"
-              viewBox="0 0 22 34"
-              fill="none"
-              stroke="white"
-              strokeWidth="2"
-              style={{ margin: index % 2 === 0 ? "-10px" : "0px" }}
+              {`${title}`}
+            </h1>
+
+            {/* texts */}
+            <div
+              className={`flex flex-col text-center sm:h-full 1024:text-left`}
             >
-              <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" />
-            </svg>
-          ))}
+              {textContent.map((text, index) => (
+                <React.Fragment key={index}>
+                  <p className="font-Archivo 320:mx-4 320:text-[12px] 320:leading-[17.8px] 768:mx-16 768:text-[13px] 1024:mx-0 1024:text-[14px] 1024:font-[300] 1024:leading-[16px] 1440:text-[16px] 1440:font-[300] 1440:leading-[18.8px] 2xl:text-[26px] 2xl:leading-[28.8px]">
+                    {text}
+                  </p>
+                  {index < textContent.length - 1 &&
+                    (number !== "17" ? <br /> : null)}
+                </React.Fragment>
+              ))}
+            </div>
+          </div>
+
+          {/* second */}
+          <div className="relative order-2 w-full 320:mt-6 320:p-4 sm:order-2 sm:w-[80%] 320:h-[860px] 1024:h-[660px] 1024:w-[640px] 1024:p-0 1440:w-[700px] 2xl:h-[760px] 2xl:w-[800px]">
+            <Image src={`${imgURL}`} blurDataURL={`${blurImg}`} layout="fill" />
+          </div>
+
+
+          {/* third */}
+          <div
+            className={`bg-${color} order-3 flex w-[6%] 1024:w-[8%] 1440:w-[8%] flex-col items-center justify-center 320:hidden sm:flex 768:hidden 1024:flex 1024:h-[600px]
+          1440:h-[680px] 2xl:h-[700px]`}
+          >
+            {filledIcons.map((index) => (
+              <svg
+                key={index}
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="28"
+                viewBox="0 0 22 34"
+                fill="white"
+                stroke="white"
+                strokeWidth="2"
+                style={{ margin: index % 2 === 0 ? "0px" : "-10px" }}
+              >
+                <path d="M1.99822 32.1138L2.22499 15.1476L20.0017 2.06227L19.775 19.0284L1.99822 32.1138Z" />
+              </svg>
+            ))}
+            {unfilledIcons.map((index) => (
+              <svg
+                key={index}
+                xmlns="http://w3.org/2000/svg"
+                width="16"
+                height="28"
+                viewBox="0 0 22 34"
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+                style={{ margin: index % 2 === 0 ? "-10px" : "0px" }}
+              >
+                <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" />
+              </svg>
+            ))}
+          </div>
+
         </div>
+
       </div>
     </>
   );
 };
 
 export default Section;
+
+
+// import React from "react";
+
+// const Section = ({ order, textContent, title, imgURL, color, number, img }) => {
+//   const totalIcons = 17;
+
+//   const filledIcons = Array.from(
+//     { length: Math.min(number, totalIcons) },
+//     (_, i) => i,
+//   );
+
+//   const unfilledIcons = Array.from(
+//     { length: totalIcons - filledIcons.length },
+//     (_, i) => i,
+//   );
+
+//   return (
+//     <>
+//       <div
+//         className={`flex h-[828px] w-full items-center justify-center bg-black ${
+//           order === "left" ? "flex-row-reverse" : "flex-row"
+//         }`}
+//       >
+//         <div className={`bg-${color} flex h-[600px] w-full`}>
+//           <div
+//             className={`flex w-full items-center justify-center text-white  ${
+//               order === "left" ? "flex-row-reverse pr-12" : "flex-row pl-12"
+//             }`}
+//           >
+//             <div>
+//               <div className="grid grid-cols-2">
+//                 <div className="flex justify-start">
+//                   <h1
+//                     className={`font-Leaguespartan mb-1 flex text-[70px] font-bold leading-[76.6%] ${
+//                       order === "left" ? "pl-12" : ""
+//                     }`}
+//                   >{`${number}`}</h1>
+//                 </div>
+//                 <div className="mr-8 flex justify-end">
+//                   <img src={`${img}`} alt="" class="h-[60px] w-[60px]" />
+//                 </div>
+//               </div>
+//               <h1
+//                 className={`font-Archivo mb-1 text-[35px] font-[400] italic ${
+//                   order === "left" ? "pl-12" : ""
+//                 }`}
+//               >{`${title}`}</h1>
+
+//               <div className={`${order === "left" ? "pl-12" : "pr-10"}`}>
+//                 {textContent.map((text, index) => (
+//                   <React.Fragment key={index}>
+//                     <p className="font-Archivo text-[14px] font-[300] leading-[19.8px]">
+//                       {text}
+//                     </p>
+//                     {index < textContent.length - 1 &&
+//                       (number !== "17" ? <br /> : null)}
+//                   </React.Fragment>
+//                 ))}
+//               </div>
+//             </div>
+//             <img src={`${imgURL}`} alt="" className="h-[660px] w-[420px]" />
+//           </div>
+//         </div>
+//         <div
+//           className={`bg-${color} flex h-[600px] w-[5%] flex-col items-center justify-center`}
+//         >
+//           {filledIcons.map((index) => (
+//             <svg
+//               key={index}
+//               xmlns="http://www.w3.org/2000/svg"
+//               width="16"
+//               height="28"
+//               viewBox="0 0 22 34"
+//               fill="white"
+//               stroke="white"
+//               strokeWidth="2"
+//               style={{ margin: index % 2 === 0 ? "0px" : "-10px" }}
+//             >
+//               <path d="M1.99822 32.1138L2.22499 15.1476L20.0017 2.06227L19.775 19.0284L1.99822 32.1138Z" />
+//             </svg>
+//           ))}
+//           {unfilledIcons.map((index) => (
+//             <svg
+//               key={index}
+//               xmlns="http://w3.org/2000/svg"
+//               width="16"
+//               height="28"
+//               viewBox="0 0 22 34"
+//               fill="none"
+//               stroke="white"
+//               strokeWidth="2"
+//               style={{ margin: index % 2 === 0 ? "-10px" : "0px" }}
+//             >
+//               <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" />
+//             </svg>
+//           ))}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Section;
