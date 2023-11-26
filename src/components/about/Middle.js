64f1fb5ad7f74e3901c5ecfa2436183998b59299
hadees <hadees621@gmail.com>
2023-11-26 20:10:16 +0500

    paras fixed

--- src/components/about/Middle.js
+++ src/components/about/Middle.js
@@ -2,7 +2,6 @@ import React from "react";
 import Image from "next/legacy/image";
 import { AboutUsURLs } from "@/util/urls";
 import { AboutUSTextOnImage } from "@/util/para";
-import { AboutUstextSections } from "@/util/para";
 import ImageTextSection from "./ImageTextSection";
 import StatsBlock from "./StatsBlock";
 
@@ -11,7 +10,7 @@ const Middle = () => {
     <>
       <div className="flex flex-col bg-black md:flex-col lg:flex-row">
         <div className="relative flex w-full flex-col lg:mt-60 lg:w-[35%]">
-          <div className="relative h-[40%] w-full ">
+          <div className="relative h-[40%] w-full">
             <Image
               src={AboutUsURLs.head}
               blurDataURL={AboutUsURLs.headBlur}
@@ -50,11 +49,31 @@ const Middle = () => {
             />
           ))}
 
-          {AboutUstextSections.map((section, index) => (
-            <p key={index} className={section.style}>
-              {section.content}
-            </p>
-          ))}
+          <p className="text-white text-center font-libre-baskerville italic font-normal leading-normal text-[13px] 768:text-[18px] p-4 1024:text-[26px] 1024:px-8 1440:text-[34px] 1440:px-16 2xl:text-[52px] 2xl:px-32">
+            Whatever your transportation needs may be, we've got you covered.
+          </p>
+
+          <p className="text-white text-right text-shadow-md font-LeagueSpartan font-bold leading-tight uppercase pt-10 320:pt-1 1024:pt-6 text-[32px] 425:text-[36px] 768:text-[65px] 1024:text-[70px] 1440:text-[80px] 2xl:text-[135px]">
+            Qualified
+            Chauffeurs
+          </p>
+
+          <p className="pt-5 text-white text-right font-Archivo mr-1 text-[7px] 768:text-[10px] 1024:text-[13px] 1024:leading-[20px] 1440:text-[17px] 2xl:text-[22px] 2xl:leading-[24px] font-light leading-[14px]">
+            At Masterpiece Limousine we understand that transportation is more than just getting from one place to another, and we strive to provide our customers with the highest level of service possible. That's why we carefully select and train our chauffeurs to deliver excellent customer service, reliability, safety, and comfort.
+            <br />
+            <br />
+            Our chauffeurs undergo thorough background checks and rigorous training to meet our strict standards. They are knowledgeable about the San Diego area and committed to providing a smooth and enjoyable ride. Whether you're traveling to or from the airport, celebrating a special occasion, or going on a group excursion, our chauffeurs are dedicated to making your transportation experience positive.
+            <br />
+            <br />
+            At Masterpiece Limousine, customer satisfaction and safety are our top priorities. We go above and beyond to ensure you have a pleasant and stress-free experience while in our care. Our fleet of luxury vehicles is well-maintained and equipped with the latest technology to ensure your safety and comfort.
+            <br />
+            <br />
+            Experience the difference with Masterpiece Limousine, where luxury meets unparalleled service.
+          </p>
+
+          <p className="text-white text-center font-libre-baskerville italic font-normal leading-normal pt-10 px-5 768:pt-12 1024:pt-16 1440:pt-36 2xl:py-32 text-[13px] 768:text-[18px] p-4 1024:text-[28px] 1024:px-10 1440:text-[34px] 1440:px-16 2xl:text-[52px] 2xl:px-36">
+            Contact us today to book your transportation needs and indulge in the ultimate luxury transportation experience.
+          </p>
 
           <div className="flex justify-center py-6">
             <button className="1024:w-[180px] 1024:h-[45px] 1024:text-[13px] 1440:w-[210px] 1440:h-[55px] 1440:text-[15px] font-Montserrat h-[38px] w-[140px] flex-shrink-0 bg-red-800 text-[11px] font-[600] uppercase tracking-wider text-white 2xl:h-[80px] 2xl:w-[250px] 2xl:text-[20px]">
--- src/util/para.js
+++ src/util/para.js
@@ -347,56 +347,6 @@ export const AboutUSTextOnImage = [
   },
 ];
 
-export const AboutUstextSections = [
-  {
-    style: "font-libre-baskerville 768:text-[18px] 1024:text-[26px] 1024:px-8  1440:text-[34px] 1440:px-16 p-4 text-center text-[13px] font-normal italic leading-normal text-white 2xl:px-32 2xl:text-[52px]",
-    content:
-      "Whatever your transportation needs may be, weve got you covered.",
-  },
-  {
-    style: "text-shadow-md font-LeagueSpartan 320:pt-1 1024:pt-6 425:text-[36px] 768:text-[65px] 1024:text-[70px] 1440:text-[80px] pt-10 text-right text-[32px] font-bold uppercase leading-tight text-white 2xl:text-[135px]",
-    content: "Qualified Chauffeurs",
-  },
-  {
-    style: "font-Archivo 768:text-[10px] 1024:text-[13px] 1024:leading-[20px] 1440:text-[17px] mr-1 pt-5 text-right text-[7px] font-light leading-[14px] text-white 2xl:text-[22px] 2xl:leading-[24px]",
-    content: (
-      <>
-        <p>
-          At Masterpiece Limousine, we understand that transportation is more
-          than just getting from one place to another, and we strive to provide
-          our customers with the highest level of service possible. That's why we
-          carefully select and train our chauffeurs to deliver excellent customer
-          service, reliability, safety, and comfort.
-        </p>
-        <p>
-          Our chauffeurs undergo thorough background checks and rigorous training
-          to meet our strict standards. They are knowledgeable about the San Diego
-          area and committed to providing a smooth and enjoyable ride. Whether
-          you're traveling to or from the airport, celebrating a special
-          occasion, or going on a group excursion, our chauffeurs are dedicated
-          to making your transportation experience positive.
-        </p>
-        <p>
-          At Masterpiece Limousine, customer satisfaction and safety are our top
-          priorities. We go above and beyond to ensure you have a pleasant and
-          stress-free experience while in our care. Our fleet of luxury vehicles
-          is well-maintained and equipped with the latest technology to ensure
-          your safety and comfort.
-        </p>
-        <p>
-          Experience the difference with Masterpiece Limousine, where luxury meets
-          unparalleled service.
-        </p>
-      </>
-    ),
-  },
-  {
-    style: "font-libre-baskerville 768:pt-12 1024:pt-16 1440:pt-36 768:text-[18px] 1024:text-[28px] 1024:px-10 1440:text-[34px] 1440:px-16 p-4 px-5 pt-10 text-center text-[13px] font-normal italic leading-normal text-white 2xl:px-36 2xl:py-32 2xl:text-[52px]",
-    content:
-      "Contact us today to book your transportation needs and indulge in the ultimate luxury transportation experience.",
-  },
-];
-
 export const NavigateListItems = [
   'Home',
   'About us',
